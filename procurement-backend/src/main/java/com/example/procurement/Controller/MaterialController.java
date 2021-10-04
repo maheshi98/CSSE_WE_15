cbafd37092492446e18bad0265b96c8a46d3dbcd
Maheshi Purnima <mashipurnima1998@gmail.com>
2021-10-04 21:39:13 +0530

    material and order services

--- procurement-backend/src/main/java/com/example/procurement/Controller/MaterialController.java
+++ procurement-backend/src/main/java/com/example/procurement/Controller/MaterialController.java
@@ -1,4 +1,29 @@
 package com.example.procurement.Controller;
 
+import com.example.procurement.Entity.MaterialsEntity;
+import com.example.procurement.Entity.OrderEntity;
+import com.example.procurement.Repository.MaterialsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(value = "*")
+@RequestMapping("/")
 public class MaterialController {
+
+    @Autowired
+    private MaterialsRepository materialsRepository;
+
+    @PostMapping(value = "/" )
+    public MaterialsEntity SaveNewOrder(@RequestBody MaterialsEntity order){
+        return materialsRepository.save(order);
+    }
+
+    @GetMapping(value = "/")
+    public List<MaterialsEntity> getAllOrder(){
+        return  materialsRepository.findAll();
+    }
+
 }
--- procurement-backend/src/main/java/com/example/procurement/Controller/OrderController.java
+++ procurement-backend/src/main/java/com/example/procurement/Controller/OrderController.java
@@ -1,4 +1,28 @@
 package com.example.procurement.Controller;
 
+import com.example.procurement.Entity.OrderEntity;
+import com.example.procurement.Repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(value = "*")
+@RequestMapping("/")
 public class OrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @PostMapping(value = "/" )
+    public OrderEntity SaveNewOrder(@RequestBody OrderEntity order){
+        return orderRepository.save(order);
+    }
+
+    @GetMapping(value = "/")
+    public List<OrderEntity> getAllOrder(){
+        return  orderRepository.findAll();
+    }
+
 }
