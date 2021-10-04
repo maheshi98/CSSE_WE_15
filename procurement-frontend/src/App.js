3f1013e3e5dda187add7aef4a3674571f1d6524b
AchiniIma <achiniimalka@gmail.com>
2021-10-04 21:34:46 +0530

    UI creation

--- procurement-frontend/src/App.js
+++ procurement-frontend/src/App.js
@@ -1,25 +1,38 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Footer from './Component/Home/Footer/index';
+import Home from './Component/Home/Home';
+import Login from './Component/Login/Login';
+import OrderTable from './Component/Orders/Orders';
+import DashBoard from './Component/DashBoard/DashBoard';
+import Details from './Component/OrderDetails/Details';
+import ApprovedOrder from './Component/ApprovedOrders/ApprovedOrder';
+import Quotation from './Component/Quatations/Quotations';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  constructor(props) {
+      super(props)
+      this.state = {}
+  }
+
+  render() {
+      return (
+          <div className= "body" style={{backgroundColor : '#ddddff'}}>
+              <BrowserRouter>
+                  <Switch>
+                      <Route exact path='/' component={Login} />
+                      <Route exact path='/dashboard' component={DashBoard} />
+                      <Route exact path='/orderInfo' component={OrderTable} />
+                      <Route exact path='/orderDetails' component={Details} />
+                      <Route exact path='/approvedOrder' component={ApprovedOrder} />
+                      <Route exact path='/submitQuotation' component={Quotation} />
+                  </Switch>
+              </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
 
-export default App;
+}
---
+++ procurement-frontend/src/Component/Home/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Button,Card} from 'react-bootstrap';
+
+
+export default class Home extends Component {
+    render() {
+        return (
+            <Card>
+            <Card.Header>Featured</Card.Header>
+            <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text>
+                    With supporting text below as a natural lead-in to additional content.
+    </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+        )
+    }
+}
