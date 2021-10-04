bbaf973b444436d815991ef3a83ae7ab9ea80197
AchiniIma <achiniimalka@gmail.com>
2021-10-05 01:19:07 +0530

    View Order Details

--- procurement-frontend/src/Services/OrderService.jsx
+++ procurement-frontend/src/Services/OrderService.jsx
@@ -8,8 +8,8 @@ class OrderService{
     // getApprovedOrders(){
     //     return axios.get('http://localhost:8080/order/getall/')
     // } 
-    // getOrderById(id ){
-    //     return axios.get('http://localhost:8080/order/getById/' + id);
-    // }
+    getOrderById(id ){
+        return axios.get('http://localhost:8080/order/getById/' + id);
+    }
 }
 export default new OrderService();
